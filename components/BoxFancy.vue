cd7f3b111de194bfa3d33d560c3536455f57e140
skwal <SkwalExe>
2024-02-28 11:05:00 +0100

    fix text size

--- components/BoxFancy.vue
+++ components/BoxFancy.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box-fancy p-[1px] bg-gradient-to-br transition duration-500 from-accent to-accent/10 rounded-md hover:scale-[1.005]">
+  <div class="box-fancy p-[2px] bg-gradient-to-br transition duration-500 from-accent to-accent/30 rounded-md hover:scale-[1.005]">
         <div class="bg-primary rounded-md">
             <div class="p-10 bg-gradient-to-tl from-black via-black to-accent/15 rounded-md">
                 <slot />
