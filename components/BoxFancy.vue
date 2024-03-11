6aed74727dff7486607ac76369c478d64fe3562f
Léopold Koprivnik <skwal@etik.com>
2024-03-11 11:55:59 +0100

    improve fancy boxes background gradient (#3)

--- components/BoxFancy.vue
+++ components/BoxFancy.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="box-fancy p-[1px] bg-gradient-to-br transition duration-500 from-accent to-accent/10 rounded-md hover:scale-[1.005]">
         <div class="bg-primary rounded-md">
-            <div class="p-10 bg-gradient-to-tl from-black via-black to-accent/15 rounded-md">
+            <div class="p-10 bg-gradient-to-tl from-violet-500/20 to-accent/20 rounded-md">
                 <slot />
             </div>
         </div>
