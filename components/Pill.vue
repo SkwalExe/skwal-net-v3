a171e105ecb0bc1b05b85e0f00fa5f69dc5fcd7a
Léopold Koprivnik <skwal@etik.com>
2024-03-11 12:56:39 +0100

    Text Size (#4)
    * fix text size
    
    * fix text size
    
    ---------
    
    Co-authored-by: skwal <SkwalExe>

--- components/Pill.vue
+++ components/Pill.vue
@@ -1,4 +1,4 @@
 <template>
     <!-- Warning : Implicitly used Tailwind Classes -->
-    <span class="inline-flex items-center text-gray-350 rounded-lg py-[1px] px-2 text-xs ring-1 ring-inset ring-gray-500/70 bg-gray-600/30 bg-opacity-15`"><slot /></span>
+    <span class="inline-flex items-center text-gray-350 rounded-lg py-[1px] px-2 ring-1 ring-inset ring-gray-500/70 bg-gray-600/30 bg-opacity-15`"><slot /></span>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,15 @@
 <template>
-    <div id="layout-body" class="bg-primary min-h-screen flex flex-col justify-between" theme="midnight-abyss">
-        <div class="flex justify-center flex-col items-center">
-            <NavBar />
-            <div class="max-w-4xl sm:p-10 p-4 flex flex-col gap-5">
-                <slot />
-            </div>
-        </div>
-        <Footer />
+  <div
+    id="layout-body"
+    class="bg-primary min-h-screen flex flex-col justify-between"
+    theme="midnight-abyss"
+  >
+    <div class="flex justify-center flex-col items-center">
+      <NavBar />
+      <div class="max-w-6xl sm:p-10 p-4 flex flex-col gap-5">
+        <slot />
+      </div>
     </div>
+    <Footer />
+  </div>
 </template>
