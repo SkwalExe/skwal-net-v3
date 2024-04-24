265e7b117e4e376a4c3d80f78f828e7c94c4644f
SkwalExe <skwal@skwal.net>
2024-04-24 11:02:12 +0000

    hanging gh pull request

--- scripts/redeploy_listener.sh
+++ scripts/redeploy_listener.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script will check for the hooks/redeploy file
 # and if it exists, it will pull the latest changes
-# from the git repository by calling deploy.sh.
+# from the git repository by calling redeploy.sh.
 
 cd /home/skwal/skwal-net-v3/
 
