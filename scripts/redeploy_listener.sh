fe2b8c9a5f24f6620da01a38834e30002a6bcecb
SkwalExe <skwal@skwal.net>
2024-04-24 10:58:46 +0000

    create redeploy listener script

---
+++ scripts/redeploy_listener.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script will check for the hooks/redeploy file
+# and if it exists, it will pull the latest changes
+# from the git repository by calling deploy.sh.
+
+cd /home/skwal/skwal-net-v3/
+
+while true; do
+  if [ -f hooks/redeploy ]; then
+    echo Redeploying...
+    rm -f hooks/redeploy
+    ./scripts/redeploy.sh
+  fi
+  sleep 5
+done
