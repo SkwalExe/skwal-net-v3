2633c0f6813d4ad5785ceac6013af6155fb6425d
SkwalExe <skwal@skwal.net>
2024-04-30 18:36:10 +0200

    small improvements

--- components/BoxFancy.vue
+++ components/BoxFancy.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="box-fancy border-[2px] border-white/25 transition duration-75 hover:border-white/30 bg-black/50 rounded-xl overflow-hidden">
-        <div class="fancy p-10 bg-gradient-to-tl from-violet-500/20 to-accent/20">
+        <div class="fancy sm:p-10 p-4 bg-gradient-to-tl from-violet-500/20 to-accent/20">
             <slot />
         </div>
     </div>
