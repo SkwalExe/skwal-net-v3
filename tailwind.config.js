0bba90b199b285dfc4e3ef2abb15d01959af7532
SkwalExe <skwal@skwal.net>
2024-05-21 14:38:13 +0200

    format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [],
-  theme: {
-    fontFamily: {
-      inter: ['Inter', 'sens-serif'],
-      poppins: ['Poppins', 'sans-serif']
+    content: [],
+    theme: {
+        fontFamily: {
+            inter: ['Inter', 'sens-serif'],
+            poppins: ['Poppins', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                // Using modern `rgb`
+                primary: 'rgb(var(--color-primary) / <alpha-value>)',
+                secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+                accent: 'rgb(var(--accent-color) / <alpha-value>)'
+            }
+        }
     },
-    extend: {
-      colors: {
-        // Using modern `rgb`
-        primary: 'rgb(var(--color-primary) / <alpha-value>)',
-        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
-        accent: 'rgb(var(--accent-color) / <alpha-value>)',
-      },
-    },
-  },
-  plugins: [],
+    plugins: []
 }
-
