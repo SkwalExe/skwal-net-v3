036baf33e6121f04301e2051bffb9988a3052502
SkwalExe <skwal@skwal.net>
2024-05-21 14:30:18 +0200

    add prettier config file

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+const config = {
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    quoteProps: 'consistent',
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    bracketSameLine: true,
+    arrowParens: 'always',
+    printWidth: 120,
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'ignore'
+}
+
+export default config
+
