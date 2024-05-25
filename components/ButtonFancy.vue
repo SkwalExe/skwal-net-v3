c3007cab9f361ba09c8aa0d90a627d4b274ec696
SkwalExe <skwal@skwal.net>
2024-05-25 16:48:29 +0200

    add winmb page

--- components/ButtonFancy.vue
+++ components/ButtonFancy.vue
@@ -1,8 +1,22 @@
 <template>
     <div
-        class="text-center text-white inline-flex bg-gradient-to-br from-accent/80 to-violet-500/80 border-white/40 border-[2px] hover:opacity-100 opacity-90 transition duration-300 rounded-md px-7 py-4 cursor-pointer hover:shadow-lg hover:shadow-accent/80">
+        :class="`fancy-button-${type} text-center text-white inline-flex bg-gradient-to-br border-white/40 border-[2px] hover:opacity-100 opacity-90 transition duration-300 rounded-md px-7 py-4 cursor-pointer`">
         <p class="w-full">
             <slot />
         </p>
     </div>
 </template>
+
+<script setup lang="ts">
+const { type } = defineProps({
+    type: {
+        type: String,
+        requird: true,
+        default: 'blue'
+    }
+})
+
+if (!['red', 'blue', 'yellow', 'green'].includes(type)) throw new Error('Invalid fancy button type')
+
+
+</script>
