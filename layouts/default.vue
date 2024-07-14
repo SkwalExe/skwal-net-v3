caa7b8db06b07cc3af702ed9106bc1f650f85650
SkwalExe <skwal@skwal.net>
2024-07-14 18:00:31 +0200

    Add banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="layout-body" class="min-h-screen flex flex-col justify-between" theme="midnight-abyss">
+    <div id="layout-body" class="min-h-screen flex flex-col justify-between">
         <div class="flex justify-center flex-col items-center">
             <NavBar />
             <div class="w-full max-w-6xl sm:p-10 flex flex-col sm:gap-5">
